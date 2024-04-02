b3389e8955e0dbb1b5516820a88b0412b5d10d81
unknown <you@example.com>
2024-04-01 19:23:37 -0500

    9.2 complete

--- counter.py
+++ counter.py
@@ -1,6 +1,21 @@
 def countDayOfTheWeek():
-    # This first line is provided for you
     file_name = input("Enter a file name: ")
+    fhand = open(file_name)
+
+    day_list = []
+    dictionary = {}
+
+    for line in fhand:
+        if line.startswith('From') and not line.startswith('From:'):
+            words = line.split()
+            day_list.append(words[2])
+    
+    for day in day_list:
+        dictionary[day] = dictionary.get(day, 0) + 1
+        
+
+    print(dictionary)
+            
 
 
 
